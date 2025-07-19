eb61c7782bc035c643af0611c8c7802a67ee790e
YouKnow <nilesh.sabale.dev@gmail.com>
2025-07-19 20:18:52 +0530

    Add files via upload

---
+++ sdes.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class sdes {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String input;
+        System.out.println("Enter Key");
+        input=sc.nextLine();
+    }
+}
